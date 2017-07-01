3daaa7a43172c75c226cc42cf191842f83b1c883
louie1966 <renedeleeuw66@gmail.com>
2017-07-02 01:15:38 +0200

    Looping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { JokeComponent } from './joke/joke.component';
+import { JokeListComponent } from './joke-list/joke-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JokeComponent
+    JokeListComponent
   ],
   imports: [
     BrowserModule
--- src/app/joke-list/joke-list.component.spec.ts
+++ src/app/joke-list/joke-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { JokeComponent } from './joke.component';
+import { JokeListComponent } from './joke-list.component';
 
-describe('JokeComponent', () => {
-  let component: JokeComponent;
-  let fixture: ComponentFixture<JokeComponent>;
+describe('JokeListComponent', () => {
+  let component: JokeListComponent;
+  let fixture: ComponentFixture<JokeListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ JokeComponent ]
+      declarations: [ JokeListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(JokeComponent);
+    fixture = TestBed.createComponent(JokeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
