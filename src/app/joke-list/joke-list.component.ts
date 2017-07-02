d929f4f4d3fa6d6109ef2e2cdbd475678f7156b0
louie1966 <renedeleeuw66@gmail.com>
2017-07-02 07:24:32 +0200

    User Interaction & Outputs

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -8,15 +8,19 @@ import { Joke } from '../joke';
 })
 
 export class JokeListComponent implements OnInit {
-  jokes: Object[];
+	jokes: Joke[];
 
-  constructor() {
-    this.jokes = [
-      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
-      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
-      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
-    ];
-  }
-  
-  ngOnInit(){  }
+	constructor() {
+		this.jokes = [
+			new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+			new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+			new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
+		];
+	}
+
+	addJoke(joke) {
+		this.jokes.unshift(joke);
+	}
+
+  ngOnInit() { }
 }
--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -12,7 +12,6 @@ export class JokeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
