6ba64715bd2bff518272331b9afc46e32fc847e4
louie1966 <renedeleeuw66@gmail.com>
2017-07-02 06:35:24 +0200

    Domain Model

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,37 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Joke } from '../joke';
 
 @Component({
   selector: 'joke-list',
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
-export class JokeListComponent implements OnInit {
 
+export class JokeListComponent implements OnInit {
   jokes: Object[];
 
   constructor() {
     this.jokes = [
-      {
-        setup: "What did the cheese say when it looked in the mirror?",
-        punchline: "Hello-Me (Halloumi)",
-        hide: true
-      },
-      {
-        setup: "What kind of cheese do you use to disguise a small horse?",
-        punchline: "Mask-a-pony (Mascarpone)",
-        hide: true
-      },
-      {
-        setup: "A kid threw a lump of cheddar at me",
-        punchline: "I thought ‘That’s not very mature’",
-        hide: true
-      },
+      new Joke("What did the cheese say when it looked in the mirror?", "Hello-me (Halloumi)"),
+      new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
+      new Joke("A kid threw a lump of cheddar at me", "I thought ‘That’s not very mature’"),
     ];
   }
+  ngOnInit(){
 
-  ngOnInit() {
-  }
-  toggle(joke) {
-    joke.hide = !joke.hide;
   }
 }
