da8bed6ef7c20838fe9c5eccb1bc2b6c91052da3
louie1966 <renedeleeuw66@gmail.com>
2017-07-18 23:17:50 +0200

    Commited working version

--- src/app/joke-form/joke-form.component.ts
+++ src/app/joke-form/joke-form.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { Joke } from '../joke';
 
 @Component({
   selector: 'joke-form',
   templateUrl: './joke-form.component.html',
-  styleUrls: ['./joke-form.component.css']
+  styleUrls: ['./joke-form.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class JokeFormComponent implements OnInit {
   @Output() jokeCreated = new EventEmitter<Joke>();
